d7a870be22e5274436e183b30a75ddbe5c0289c5
Zachary-F201 <zjmf201@exeter.ac.uk>
2025-02-07 13:09:47 +0000

    read input file completed

--- Main.py
+++ Main.py
@@ -5,8 +5,20 @@
 #Append that to a list/dictionary 
 
 def readinputfile(file = "input1.txt"):
-    with open(file,"r") as r:
-        file = r.readlines()
-        print(file)
+    # This function gives three pieces of data: the total number floors, the capacity of the lift and the requests of each floor.
+    global floors, capactiy, requests
+    with open(file,"r") as r: #Creates a pointer for the file
+        files = r.readlines()
+        floors = int(str(files[1])[:files[1].find(",")]) #adjustsable for floors above 9
+        capacity = int(str(files[1])[files[1].find(" "):-1])
+        requests = files[4:]
+        for i in range(len(requests)):
+            requests[i] = (str(requests[i])[str(requests[i]).find(":")+1:-1]).split(",") #Makes each line an index in the array
+        for x in range(len(requests)):
+            for y in range(len(requests[x])):
+                try: # This will make each string value an integer unless it is blank, in which case it is given a None value
+                    requests[x][y] = int(requests[x][y])
+                except ValueError:
+                    requests[x][y] = None
 
 readinputfile()
