b8b9a59ce2be2b60a348728c7fa7aced2b9a1869
Zachary-F201 <zjmf201@exeter.ac.uk>
2025-02-26 11:32:13 +0000

    started plotting graphs

--- plotting_graphs.py
+++ plotting_graphs.py
@@ -4,4 +4,13 @@ from matplotlib import pyplot as plt
 with open("data.txt","r") as r:
     file = r.readlines()
     for i in range(3):
-        for x in range(np.array())
+        x_values = []
+        y_values = []
+        data = eval(file[1+i])
+        for t in range(len(data)):
+            x_values.append(int(data[t][1]))
+            y_values.append(int(data[t][0]))
+        plt.plot(x_values, y_values, label = "MYLIFT"+str(t))
+
+    plt.legend()
+    plt.show()
